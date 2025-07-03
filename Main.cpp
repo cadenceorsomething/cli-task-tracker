02a58984f6f50e309c32777ca99cf22225484315
Cad <aninterestingnameidk0@gmail.com>
2025-07-03 14:31:36 +0300

    i found the problem, some jsons didnt contain id which made it cause an error. all jsons now contain id right after being created.

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,8 @@ using json = nlohmann::json;
 int main(int argc,char *argv[]) {
 	std::string file_name = "json_file";
 	json data = cad::load_data(file_name);
-	
+
+
 	if (argc < 2) {
 		std::cout << "...you were supposed to enter a <COMMAND>" << std::endl;
 		return 1;
@@ -24,13 +25,13 @@ int main(int argc,char *argv[]) {
 		std::string description = argv[2];
 		cad::add_task(data, description);
 	}
-
-
+	if (command == "list") {
+		cad::list_tasks(data);
+	}
 
 
 	cad::sort_tasks	(data);
 	cad::save_data	(data, file_name);
-	cad::list_tasks	(data);
 
 	return 0;
 }
