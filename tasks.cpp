8809728d691fbb74fa1bf4cddafdb77312ffacb2
Cad <aninterestingnameidk0@gmail.com>
2025-07-03 14:48:12 +0300

    js changed the name and implemented 'complete' command

---
+++ tasks.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "json.hpp"
+#include "Cad.h"
+
+
+using json = nlohmann::json;
+
+
+int main(int argc,char *argv[]) {
+	std::string file_name = "json_file";
+	json data = cad::load_data(file_name);
+
+
+	if (argc < 2) {
+		std::cout << "...you were supposed to enter a <COMMAND>" << std::endl;
+		return 1;
+	}
+
+	std::string command = argv[1];
+
+	if (command == "add") {
+		if (argc < 3) {
+			std::cerr << "...missing task description" << std::endl;
+			return 1;
+		}
+		std::string description = argv[2];
+		cad::add_task(data, description);
+	}
+	if (command == "list") {
+		cad::list_tasks(data);
+	}
+	if (command == "completed") {
+		if (argc < 3) {
+			std::cerr << "...missing id" << std::endl;
+			return 1;
+		}
+
+		int id = std::stoi(argv[2]);
+		if (!cad::cross_out(data, id)) {
+			std::cerr << "...couldn't find id" << std::endl;
+			return 1;
+		}
+	}
+
+	cad::sort_tasks	(data);
+	cad::save_data	(data, file_name);
+
+	return 0;
+}
