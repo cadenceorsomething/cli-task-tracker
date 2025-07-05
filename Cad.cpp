70182031a075722c283cecf8824f8de5cd72dfe5
Cad <aninterestingnameidk0@gmail.com>
2025-07-05 15:26:06 +0300

    this is the final version of this project, i added time tracking. now it tracks when a task was made and when it was updated.

--- Cad.cpp
+++ Cad.cpp
@@ -1,14 +1,92 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <ctime>
 #include "json.hpp"
 #include "Cad.h"
 
+// global names of the task categories
+const std::string
+_tasks			= "tasks",
+_name			= "name",
+_id				= "id",
+_done			= "done",
+_time_created	= "time created",
+_time_updated	= "time updated";
+
 
 namespace cad {
-
 	using json = nlohmann::json;
 
+	// time functions
+	std::string get_date	(const std::chrono::system_clock::time_point& now) {
+		using namespace std::chrono;	// to avoid verbosity
+
+		// takes in current time and rounds it to the nearest whole day, removing the time of day portion.
+		auto today = floor<days>(now);
+		year_month_day date = year_month_day{ today };
+
+		std::ostringstream oss;
+		oss << static_cast<unsigned>(date.month()) << "/"
+			<< static_cast<unsigned>(date.day()) << "/"
+			<< static_cast<int>(date.year());
+
+		return oss.str();
+	}
+	std::string get_time	(const std::chrono::system_clock::time_point& now) {
+		using namespace std::chrono;
+		std::time_t t = system_clock::to_time_t(now);
+
+		std::tm local_tm;
+
+		if (localtime_s(&local_tm, &t) != 0) return "...invalid time";
+
+		std::ostringstream oss;
+		oss << std::put_time(&local_tm, "%I:%M %p");
+		return oss.str();
+	}
+	std::string current_time() {
+		std::ostringstream oss;
+		auto now = std::chrono::system_clock::now();
+
+		oss << get_date(now) << " " << get_time(now);
+
+		return oss.str();
+	}
+
+
+
+
+	// literally everything else
+	void		display				(const json& tasks, const int& i, const bool show_progress = false) {
+		const json& task = tasks[i];
+
+
+		std::cout << task[_id] << ": " << task[_name];
+		if (show_progress == true) std::cout << "    -status: " << task[_done];
+		std::cout << std::endl;
+
+
+
+		if (task.contains(_time_created)) {
+			std::cout << "    -time created: " << task[_time_created];
+		}
+		else if (task.contains(_time_updated)) {
+			std::cout << "    -time updated: " << task[_time_updated];
+		}
+		std::cout << std::endl;
+
+		bool both_exist = task.contains(_time_updated) && task.contains(_time_created);
+
+		if (both_exist && (task[_time_updated] != task[_time_created])) {
+			std::cout << "    -time updated: " << task["time updated"];
+		}
+
+		std::cout << std::endl;
+	}
 	json		load_data			(const std::string filename) {
 		std::ifstream in_file(filename);
 		json data;
@@ -26,21 +104,15 @@ namespace cad {
 		std::ofstream out_file(filename);
 		out_file << data.dump(4);
 	}
-	std::string input_task_name		() {
-		std::string task;
-		std::cout << "Enter task name: ";
-		std::getline(std::cin, task);
-		return task;
-	}
 	int			generate_id			(const json& data) {
-		const auto& tasks = data["tasks"];
+		const json& tasks = data[tasks];
 		int id = 1;
 
 		while (true) {
 			bool found = false;
 
 			for (const auto& task : tasks) {
-				if (task.contains("id") && task["id"] == id) {
+				if (task.contains(_id) && task[_id] == id) {
 					found = true;
 					break;
 				}
@@ -53,19 +125,26 @@ namespace cad {
 		}
 	}
 	void		add_task			(json& data, std::string task) {
-		data["tasks"].push_back({ {"id", generate_id(data)}, {"name", task}, {"done", "to do"}});
+		std::string now = current_time(); //so they can both match & less computation
+
+		data[_tasks].push_back({ 
+			{_id, generate_id(data)}, 
+			{_name, task}, 
+			{_done, "to do"},
+			{_time_created, now},
+			{_time_updated, now}});
 	}
 	void		list_tasks			(const json& data) {
-		const auto& tasks = data["tasks"];
+		const auto& tasks = data[_tasks];
 		for (size_t i = 0; i < tasks.size(); ++i) {
-			std::cout << tasks[i]["id"] << ": " << tasks[i]["name"] << "    -progress: " << tasks[i]["done"] << std::endl;
+			display(tasks, i, true);
 		}
 	}
 	bool		delete_task			(json& data, int id_to_delete) {
-		auto& tasks = data["tasks"];
+		auto& tasks = data[_tasks];
 
 		for (auto it = tasks.begin(); it != tasks.end(); ++it) {
-			if (it->contains("id") && (*it)["id"] == id_to_delete) {
+			if (it->contains(_id) && (*it)[_id] == id_to_delete) {
 				tasks.erase(it);
 				return true;
 			}
@@ -77,6 +156,7 @@ namespace cad {
 		for (auto& task : tasks) {
 			if (task.contains("id") && task["id"] == id) {
 				task["done"] = "done";
+				task["time updated"] = current_time();
 				return true;
 			}
 
@@ -88,6 +168,7 @@ namespace cad {
 		for (auto& task : tasks) {
 			if (task.contains("id") && task["id"] == id) {
 				task["done"] = "in progress";
+				task["time updated"] = current_time();
 				return true;
 			}
 
@@ -100,6 +181,7 @@ namespace cad {
 			if (task.contains("id") && task["id"] == id) {
 				task["done"] = "to do";
 				return true;
+				task["time updated"] = current_time();
 			}
 
 		}
@@ -111,6 +193,8 @@ namespace cad {
 		for (auto& task : tasks) {
 			if (task.contains("id") && task["id"] == id) {
 				task["name"] = new_name;
+				task["time updated"] = current_time();
+
 				return true;
 			}
 		}
@@ -147,24 +231,25 @@ namespace cad {
 	void		list_done			(const json& data) {
 		const auto& tasks = data["tasks"];
 		for (size_t i = 0; i < tasks.size(); ++i) {
-			if (tasks[i]["done"] == "done")
-				std::cout << tasks[i]["id"] << ": " << tasks[i]["name"] << std::endl;
+			if (tasks[i]["done"] == "done") {
+				display(tasks, i);
+			}
 		}
 	}
 	void		list_in_progress	(const json& data) {
 		const auto& tasks = data["tasks"];
 		for (size_t i = 0; i < tasks.size(); ++i) {
-			if (tasks[i]["done"] == "in progress")
-				std::cout << tasks[i]["id"] << ": " << tasks[i]["name"] << std::endl;
+			if (tasks[i]["done"] == "in progress") {
+				display(tasks, i);
+			}
 		}
 	}
 	void		list_to_do			(const json& data) {
 		const auto& tasks = data["tasks"];
 		for (size_t i = 0; i < tasks.size(); ++i) {
-			if (tasks[i]["done"] == "to do")
-				std::cout << tasks[i]["id"] << ": " << tasks[i]["name"] << std::endl;
+			if (tasks[i]["done"] == "to do") {
+				display(tasks, i);
+			}
 		}
 	}
-
-
 }
--- task-cli.cpp
+++ task-cli.cpp
@@ -9,7 +9,8 @@ int main(int argc,char *argv[]) {
 	std::string file_name = "json_file";
 	json data = cad::load_data(file_name);
 	
-
+	
+	// ig there is only one argument (calling out the program) the program will just end without doing anything
 	if (argc < 2) {
 		std::cout << "...you were supposed to enter a <COMMAND>" << std::endl;
 		return 1;
@@ -17,15 +18,13 @@ int main(int argc,char *argv[]) {
 
 	std::string command = argv[1];
 
-
-	if (command == "add")			return cmd::handle_add(data, file_name, argc, argv);
-	else if (command == "list")			return cmd::handle_list(data, argc, argv);
-	else if (command == "mark-done")	return cmd::handle_done(data, file_name, argc, argv);
-	else if (command == "delete")		return cmd::handle_delete(data, file_name, argc, argv);
-	else if (command == "update")		return cmd::handle_update(data, file_name, argc, argv);
-	else if (command == "mark-to-do")	return cmd::handle_to_do(data, file_name, argc, argv);
-	else if (command == "mark-in-progress")	return cmd::handle_in_progress(data, file_name, argc, argv);
-	else								return cmd::default_message	();
-
-	return 0;
+	// if there is a second argument, it will be taken to a function of these in 'command.cpp'
+	if		(command == "add")				return cmd::handle_add			(data, file_name, argc, argv);
+	else if (command == "list")				return cmd::handle_list			(data, argc, argv);
+	else if (command == "mark-done")		return cmd::handle_done			(data, file_name, argc, argv);
+	else if (command == "delete")			return cmd::handle_delete		(data, file_name, argc, argv);
+	else if (command == "update")			return cmd::handle_update		(data, file_name, argc, argv);
+	else if (command == "mark-to-do")		return cmd::handle_to_do		(data, file_name, argc, argv);
+	else if (command == "mark-in-progress")	return cmd::handle_in_progress	(data, file_name, argc, argv);
+	else									return cmd::default_message		();
 }
